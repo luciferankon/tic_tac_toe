87b5ed1cf5a2afff47e2f3fd5b8fa1ed06927831
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-11-23 18:11:03 +0530

    indent test

--- test/libTest.js
+++ test/libTest.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const lib = require('../src/lib.js');
 
 describe('init board',function(){
-    it('should return a 3x3 array containing all null values',function(){
-          assert.deepEqual(lib.initBoard(),[[null,null,null],[null,null,null],[null,null,null]]);
-        });
+  it('should return a 3x3 array containing all null values',function(){
+    assert.deepEqual(lib.initBoard(),[[null,null,null],[null,null,null],[null,null,null]]);
+  });
 });
