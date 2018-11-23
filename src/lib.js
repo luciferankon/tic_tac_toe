be78c359db98fdb22d4306eded0762358840b346
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-11-23 18:10:37 +0530

    Refactor initBoard

--- src/lib.js
+++ src/lib.js
@@ -1,4 +1,10 @@
+//Initialize game board
 const initBoard = function(){
-  return [[null,null,null],[null,null,null],[null,null,null]];
+  return [ 
+    [null,null,null],
+    [null,null,null],
+    [null,null,null]
+  ];
 }
+
 exports.initBoard = initBoard;
