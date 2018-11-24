c0589247beaf0d8a9918d0c10171bc77adf612b7
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-11-24 13:03:13 +0530

    Introduce placeSymbol function.
    It places the symbol on the specified position on the board.

--- src/lib.js
+++ src/lib.js
@@ -7,4 +7,13 @@ const initBoard = function(){
   ];
 }
 
+//Insert symbol into board
+const placeSymbol = function(position, symbol, board){
+  let row = parseInt((position - 1) / 3);
+  let column = (position - 1) % 3;
+  board[row][column] = symbol;
+  return board;
+}
+  
 exports.initBoard = initBoard;
+exports.placeSymbol = placeSymbol;
--- test/libTest.js
+++ test/libTest.js
@@ -1,8 +1,27 @@
 const assert = require('assert');
-const lib = require('../src/lib.js');
+const {initBoard,
+  placeSymbol
+} = require('../src/lib.js');
 
 describe('init board',function(){
   it('should return a 3x3 array containing all null values',function(){
-    assert.deepEqual(lib.initBoard(),[[null,null,null],[null,null,null],[null,null,null]]);
+    assert.deepEqual(initBoard(),[[null,null,null],[null,null,null],[null,null,null]]);
+  });
+});
+
+describe('place symbol',function(){
+  let board;
+  beforeEach(function(){
+    board = [[null,null,null],[null,null,null],[null,null,null]];
+  });
+
+  it('should return a 3x3 array with specified symbol at the specified position',function(){
+    assert.deepEqual(placeSymbol(2, "X", board), [[null,"X",null],[null,null,null],[null,null,null]]);
+  });
+
+  it("should change original array", function(){
+    assert.deepEqual(placeSymbol(2, "X", board), [[null,"X",null],[null,null,null],[null,null,null]]);
+    assert.deepEqual(placeSymbol(5, "X", board), [[null,"X",null],[null,"X",null],[null,null,null]]);
+    assert.deepEqual(placeSymbol(8, "X", board), [[null,"X",null],[null,"X",null],[null,"X",null]]);
   });
 });
